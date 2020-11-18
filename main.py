c19dca3f67760964a63cd35a0c2001f9de8b4818
contato.gallodev@gmail.com <contato.gallodev@gmail.com>
2020-11-18 20:52:05 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+worldMessage = 'Eu oodeiooo mulheres'
+print('hello world! '+worldMessage) 
+inpuMessage = input('Voce realmente odeia mulheres?')
+if inpuMessage == 'sim': 
+    print('iiiiiiiiiiiiiiiiiiii tu que deixa')
+else: 
+    print('Bem vindo ao clube')
+
+
